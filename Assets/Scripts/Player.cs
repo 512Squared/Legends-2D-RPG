12451b9813960069facda289350e51466f061f97
MarkHelsinki <mark.helsinki@gmail.com>
2021-09-01 01:07:39 +0300

    Game dev stuff
    Facing right direction
    Colliding with Objects
    Switching scenes
    Scene manager
    Singleton pattern

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,14 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static Player instance;
 
     [SerializeField] Rigidbody2D playerRigidBody;
-    [SerializeField] float moveSpeed = 4;
+    [SerializeField] int moveSpeed = 1;
+    [SerializeField] Animator playerAnimator;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        else 
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
@@ -19,8 +31,20 @@ public class Player : MonoBehaviour
     {
         float horizontalMovement = Input.GetAxisRaw("Horizontal");
         float verticalMovement = Input.GetAxisRaw("Vertical");
-        
+                
         playerRigidBody.velocity = new Vector2(horizontalMovement, verticalMovement) * moveSpeed;
 
+        playerAnimator.SetFloat("movementX", playerRigidBody.velocity.x);
+        playerAnimator.SetFloat("movementY", playerRigidBody.velocity.y);
+
+        if(horizontalMovement == 1 || horizontalMovement == -1 || verticalMovement == 1 || verticalMovement == -1)
+        {
+            playerAnimator.SetFloat("lastX", horizontalMovement);
+            playerAnimator.SetFloat("lastY", verticalMovement);
+        }
+
     }
+
+
+
 }
