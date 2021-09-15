32515efe5ca79c8231525b677f808d76728573bf
MarkHelsinki <mark.helsinki@gmail.com>
2021-09-15 17:16:22 +0300

    Added dialogue panel

---
+++ Assets/Birly Girly/Demo_GameResource/Script/ColliderEvent_Sender.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderEvent_Sender : MonoBehaviour {
+    private CharacterController_2D m_parent;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+    
+        //if (m_parent.Once_Attack)
+        //    return;
+
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<CharacterController_2D>().Hitted();
+            Debug.Log("hit::" + other.name);
+
+        }
+        else if (other.CompareTag("Monster"))
+        {
+
+
+            other.GetComponent<WoodDoll_Mgr>().Hitted();
+            Debug.Log("hit::" + other.name);
+
+
+        }
+
+    
+    }
+
+
+}
