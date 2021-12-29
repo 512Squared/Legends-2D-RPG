25383c18dd7b082aa4e99d09c0f6316d31e294b4
MarkHelsinki <mark.helsinki@gmail.com>
2021-12-29 21:10:24 +0200

    build

--- Assets/Scripts/SecretShopSection.cs
+++ Assets/Scripts/SecretShopSection.cs
@@ -35,7 +35,7 @@ public class SecretShopSection : MonoBehaviour
     void FixedUpdate()
     {
 
-  
+
         Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
         RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
 
@@ -53,12 +53,29 @@ public class SecretShopSection : MonoBehaviour
                     hitNumber++;
                 }
             }
+
+            else if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+                {
+                    Debug.Log(hit.transform.name);
+                    if (hitNumber < 1)
+                    {
+                        OnClick.Invoke();
+                        StartCoroutine(ResetBell());
+                        SetImageBool();
+                        hitNumber++;
+                    }
+                }
+            }
         }
-        
+
         else
         {
             return;
         }
+
 
     }
 
