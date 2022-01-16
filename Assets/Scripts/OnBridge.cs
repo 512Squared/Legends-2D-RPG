89a62aca2110ca3e6a5417a0adfcd18f146e600e
MarkHelsinki <mark.helsinki@gmail.com>
2022-01-16 22:25:35 +0200

    Fixes + build
    Upgraded bridges to work off one script. Added buffers and failsafe.
    
    Fixed layers in Dungeon. Layers were missing.

--- Assets/Scripts/OnBridge.cs
+++ Assets/Scripts/OnBridge.cs
@@ -6,33 +6,74 @@ using UnityEngine.Tilemaps;
 
 public class OnBridge : MonoBehaviour
 {
+    [SerializeField] EdgeCollider2D top_right_edge;  // to stop going off bridge - link in inspector, same for both sides
+    [SerializeField] EdgeCollider2D bottom_left_edge;
 
-    [SerializeField] EdgeCollider2D edge1;
-    [SerializeField] EdgeCollider2D edge2;
+    [SerializeField] Tilemap bridgeTilemap; // tilemap with under and over parts of bridge, on sorting layer above player
 
-    [SerializeField] Tilemap bridgeTilemap;
+    [SerializeField] OnBridge otherSide; // two sides - link in inspector
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] GameObject buffer; //entry from under
+
+    bool isOnBridge = false;
+
+    private void Start()
     {
-        //bridgeTilemap = GameObject.FindGameObjectWithTag("HomestreadBridge").GetComponent<Tilemap>();
-
+        top_right_edge.enabled = false;
+        bottom_left_edge.enabled = false;
     }
+
 
     public void OnTriggerEnter2D(Collider2D col)
     {
 
-        bridgeTilemap = GameObject.FindGameObjectWithTag("HomestreadBridge").GetComponent<Tilemap>();
+        Debug.Log($"This tag: {this.gameObject.tag} | Other's tag: {col.tag}");
 
-        if (col.gameObject.tag == "Player")
+        if (col.tag == "Player" && this.gameObject.tag != "failsafe")
         {
-            bridgeTilemap.GetComponent<Renderer>().sortingOrder = -3;
-            edge1.enabled = true;
-            edge2.enabled = true;
-
-            Debug.Log("OnCollision for bridge triggered - ON bridge" + bridgeTilemap.GetComponent<Renderer>().sortingOrder);
+            BridgeActivation();
         }
 
+        if (col.tag == "Player" && this.gameObject.tag == "failsafe")
+        {
+            Debug.Log("Failsafe - is OFF Bridge");
+            bridgeTilemap.GetComponent<Renderer>().sortingOrder = +3;
+            buffer.GetComponent<BoxCollider2D>().enabled = true;
+            otherSide.buffer.GetComponent<BoxCollider2D>().enabled = true;
+            top_right_edge.enabled = false;
+            bottom_left_edge.enabled = false;
+        }
     }
 
+    public void BridgeActivation()
+    {
+        isOnBridge = !isOnBridge;
+        otherSide.isOnBridge = !otherSide.isOnBridge;
+
+        top_right_edge.enabled = !top_right_edge.enabled;
+        //otherSide.top_right_edge.enabled = !otherSide.top_right_edge.enabled;
+
+        bottom_left_edge.enabled = !bottom_left_edge.enabled;
+        //otherSide.bottom_left_edge.enabled = !otherSide.bottom_left_edge.enabled;
+
+        if (isOnBridge == false)
+        {
+            Debug.Log("Is OFF Bridge");
+            bridgeTilemap.GetComponent<Renderer>().sortingOrder = +3;
+            buffer.GetComponent<BoxCollider2D>().enabled = true;
+            otherSide.buffer.GetComponent<BoxCollider2D>().enabled = true;
+
+        }
+
+        else if (isOnBridge == true)
+        {
+            Debug.Log("Is ON Bridge");
+            bridgeTilemap.GetComponent<Renderer>().sortingOrder = -3;
+            buffer.GetComponent<BoxCollider2D>().enabled = false;
+            otherSide.buffer.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
 }
+
+
+
